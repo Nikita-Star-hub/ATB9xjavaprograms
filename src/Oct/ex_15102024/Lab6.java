827cb5b74857ba0ca4b81390de646c14124c73c0
LENOVO <nkta.mahajan17@gmail.com>
2024-10-15 15:45:25 +0530

    task2 15/10/2024

---
+++ src/Oct/ex_15102024/Lab6.java
@@ -0,0 +1,11 @@
+package Oct.ex_15102024;
+
+public class Lab6 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println(++a + a++ + a++);//(11,11),(11,12),(12,13)->34
+
+        System.out.println(a);//13
+    }
+}
