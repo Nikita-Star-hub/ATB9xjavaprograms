8f9221ac3a8ce7d7d181b67d28aaeb0035bb2ae5
LENOVO <nkta.mahajan17@gmail.com>
2024-10-15 15:31:21 +0530

    increment operator

--- src/Oct/ex_09092024/Lab002.java
+++ src/Oct/ex_09092024/Lab002.java
@@ -1,4 +1,26 @@
 package Oct.ex_09092024;
 
 public class Lab002 {
+    public static void main(String[] args) {
+        //primitive data types
+       byte b=10; // 8 bits -1 Byte
+         short s=100;//16 bits-2 Bytes
+        int i=200;//32 bits - 4 Bytes
+        long phone_number = 909804186; //64 bits -8 Bytes
+        char grade ='A';//A,b,c,d,el->ASCII Value-int-assigned//16 bits- 2 Bytes
+        float f1 = 3.14F;//32 bits- 4 Bytes
+        double d = 118.9876543;//64 bites -8 Bytes
+        //Non primitive data type (user defined)
+        String Name = "Pransh";//string -is bunch of char( collections of char)
+        //Array class - user data type 64 bits - 8 Byte
+        //store distance between moon and earth
+        long distance_between_moon_and_earth= 384400000l; //in M //this l is use  for meters
+        //if i want in a points we can use double
+        double distance_between_moon_and_earth2= 384400000.076;
+        byte age=122;
+        int age2 =100; // wasting of memory-2147683484488;
+        //  but int double- 99% used
+
+
+    }
 }
